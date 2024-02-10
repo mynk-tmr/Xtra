954af1042d84ecf197a44f29c16c65239a821cfb
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-02-10 20:05:12 +0530

    test: add auth tests sharing same browser context
    tests to validate user registration, login and logout flow have been implemented fully

--- e2e/tests/auth.spec.js
+++ e2e/tests/auth.spec.js
@@ -1,9 +1,19 @@
 // @ts-check
-import { test, expect } from "@playwright/test";
-const CLIENT_URL = "http://localhost:3000/";
+import { test } from "@playwright/test";
 
-test("unregistered user cannot login", async ({ page }) => {
-  await page.goto(CLIENT_URL);
+let page = null;
+
+test.beforeAll(async ({ browser }) => {
+  const context = await browser.newContext();
+  page = await context.newPage();
+  await page.goto("http://localhost:3000/");
+});
+
+test.afterAll(async ({ browser }) => {
+  browser.close();
+});
+
+test("unregistered user cannot login", async () => {
   await page.getByRole("link", { name: "Sign In", exact: true }).click();
   await page.locator("[name=email]").fill("anon@test.com");
   await page.locator("[name=password]").fill("anon123");
@@ -11,9 +21,7 @@ test("unregistered user cannot login", async ({ page }) => {
   await page.locator(".Toastify__toast--error");
 });
 
-test("registerd user can login", async ({ page }) => {
-  await page.goto(CLIENT_URL);
-  await page.getByRole("link", { name: "Sign In", exact: true }).click();
+test("registerd user can login", async () => {
   await page.locator("[name=email]").fill("play1@test.com");
   await page.locator("[name=password]").fill("play123");
   await page.getByRole("button", { name: "Sign In" }).click();
@@ -21,7 +29,13 @@ test("registerd user can login", async ({ page }) => {
   await page.getByRole("link", { name: "Profile", exact: true }).click();
 });
 
-test.skip("unregistered user can register new account", async ({ page }) => {
+test("logged in user can logout", async () => {
+  await page.getByRole("button", { name: "Logout" }).click();
+  await page.locator(".Toastify__toast--success");
+  await page.getByRole("link", { name: "Sign In", exact: true }).click();
+});
+
+test("unregistered user can register new account", async () => {
   await page.getByRole("link", { name: "Register" }).click();
   await page.locator("[name=firstName]").fill("Random");
   await page.locator("[name=lastName]").fill("Number");
@@ -31,10 +45,4 @@ test.skip("unregistered user can register new account", async ({ page }) => {
   await page.getByRole("button", { name: "Create Account" }).click();
   await page.locator(".Toastify__toast--success");
   await page.getByRole("link", { name: "Profile", exact: true }).click();
-});
-
-test.skip("logged in user can logout", async ({ page }) => {
-  await page.getByRole("button", { name: "Logout" }).click();
-  await page.locator(".Toastify__toast--success");
-  await page.getByRole("link", { name: "Sign In", exact: true });
 });
