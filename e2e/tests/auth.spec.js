a0ae7d90966c47ce4a36f3576527fdc72d36111f
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-02-22 20:14:06 +0530

    refactor: improve auth test readability

--- e2e/tests/auth.spec.js
+++ e2e/tests/auth.spec.js
@@ -1,37 +1,30 @@
 // @ts-check
 import { expect, test } from "@playwright/test";
 
-let page = null;
-
-test.beforeAll(async ({ browser }) => {
-  const context = await browser.newContext();
-  page = await context.newPage();
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/");
+  await page.getByRole("link", { name: "Sign In", exact: true }).click();
 });
 
-test("unregistered user cannot login", async () => {
-  await page.getByRole("link", { name: "Sign In", exact: true }).click();
+test("unregistered user cannot login", async ({ page }) => {
   await page.locator("[name=email]").fill("anon@test.com");
   await page.locator("[name=password]").fill("anon123");
   await page.getByRole("button", { name: "Sign In" }).click();
   await expect(page.getByText(/invalid/i)).toHaveCount(1);
 });
 
-test("registerd user can login", async () => {
+test("registerd user can login and logout", async ({ page }) => {
   await page.locator("[name=email]").fill("kiran@123.com");
   await page.locator("[name=password]").fill("kiran123");
   await page.getByRole("button", { name: "Sign In" }).click();
   await expect(page.getByText(/signed in/i)).toHaveCount(1);
   await page.getByRole("link", { name: "Profile", exact: true }).click();
-});
-
-test("logged in user can logout", async () => {
   await page.getByRole("button", { name: "Logout" }).click();
   await expect(page.getByText(/signed out/i)).toHaveCount(1);
   await page.getByRole("link", { name: "Sign In", exact: true }).click();
 });
 
-test("unregistered user can register new account", async () => {
+test("unregistered user can register new account", async ({ page }) => {
   await page.getByRole("link", { name: "Register" }).click();
   await page.locator("[name=firstName]").fill("Random");
   await page.locator("[name=lastName]").fill("Number");
